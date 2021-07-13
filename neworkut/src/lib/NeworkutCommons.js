067f85dd1da084ffc6c5e0a24974815973adc2c1
Breno Estrella <brenogbestrella@gmail.com>
2021-07-13 14:14:09 -0300

    Commit aula 2

--- neworkut/src/lib/NeworkutCommons.js
+++ neworkut/src/lib/NeworkutCommons.js
@@ -162,11 +162,12 @@ AlurakutMenu.Logo = styled.img`
   height: 34px;
 `;
 
-function AlurakutMenuProfileSidebar({ githubUser }) {
+function AlurakutMenuProfileSidebar() {
+  const githubUser = 'brenogbestrella';
   return (
     <div className="alurakutMenuProfileSidebar">
       <div>
-        <img src={`https://github.com/${githubUser}.png`} style={{ borderRadius: '8px' }} />
+        <img src={`https://github.com/${githubUser}.png`} style={{ borderRadius: '8px'}} />
         <hr />
         <p>
           <a className="boxLink" href={`/user/${githubUser}`}>
